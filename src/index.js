446dd3e951a964b9a65b8f4d43f6b9a44fe2f8aa
Will Sokolowski <will.sokolowski@gmail.com>
2019-03-15 16:11:53 -0700

    switch for handling actions

--- src/index.js
+++ src/index.js
@@ -11,10 +11,30 @@ const initialState = {
 
 function reducer(state = initialState, action) {
     console.log('reducer', state, action);
-    return state;
+
+    switch(action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+            return state;
+    }
 }
 
 const store = createStore(reducer);
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "RESET" });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
